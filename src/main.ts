47f12955b283c10d34172731cafe2aa0d7f7fd9c
zs <3199441913@qq.com>
2026-04-24 18:59:46 +0800

    初始化pinia&安装mockjs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
@@ -11,6 +12,8 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+const pinia = createPinia()
+app.use(pinia)
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
