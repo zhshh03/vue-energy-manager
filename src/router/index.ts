802b9c045fb90405e286598eb652a5e536040785
zs <3199441913@qq.com>
2026-04-24 14:09:28 +0800

    配置了@符

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,12 @@ const routes:RouteRecordRaw[] = [
   {
     path: '/',
     name:'Home',
-    component:() => import('../views/Home.vue')
+    component:() => import('@/views/Home.vue')
   },
   {
     path: '/about',
     name:'About',
-    component:() => import('../views/About.vue')
+    component:() => import('@/views/About.vue')
   },
 ]
 
