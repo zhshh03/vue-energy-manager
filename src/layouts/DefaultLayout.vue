da1163395388ee9f00a57159671fa35b092371b5
zs <3199441913@qq.com>
2026-04-25 12:04:18 +0800

    主页面搭建完成

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    <h1>主页面</h1>
-    <el-button type="primary" plain>Primary</el-button>
-    <div>
-    <el-icon :size="20" :color="'red'">
-      <Edit />
-    </el-icon>
-    </div>
+   <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
+
+
+<style lang="less" scoped>
+.el-aside{
+  height: 100vh;
+  background-color: white;
+  box-shadow: 10px 0 8px -2px rgba(0, 0, 0, 0.2);
+}
+.el-main{
+  overflow: auto;
+  background-color: #f0f2f5;
+}
+
+.el-header{
+  padding: 0 !important;
+}
+
+</style>
