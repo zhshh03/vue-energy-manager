b054ea971e19f29563f98d11ba447abf52baa9e2
zs <3199441913@qq.com>
2026-04-29 23:35:00 +0800

    引入高德地图Api&地图基本结构搭建

---
+++ src/components/map/mapContain.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script setup lang="ts">
+import AMapLoader from "@amap/amap-jsapi-loader";
+// import { onMounted, onUnmounted } from "vue";
+
+let map: any = null;
+AMapLoader.load({
+  key: "ef1ed0ee0ba70bf7f8761a46c26e04a7",
+  version: "1.4.15",
+  plugins: [],
+})
+  .then((AMap) => {
+    map = new AMap.Map("container", {
+      // 设置地图容器id
+      viewMode: "3D", // 是否为3D地图模式
+      zoom: 5, // 初始化地图级别
+      center: [110.327428, 32.90923], // 初始化地图中心点位置
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+</script>
+
+<style scoped>
+#container {
+  width: 100%;
+  height: 80vh;
+}
+</style>
--- src/views/Map/map.vue
+++ src/views/Map/map.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>电子地图</div>
+  <el-row :gutter="20">
+    <el-col :span="16">
+      <el-card>
+        <map-contain></map-contain>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
+import mapContain from "@/components/map/mapContain.vue";
 </script>
