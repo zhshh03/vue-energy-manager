23e7c80122ec50ebc525c94c24d07fee74f7bffa
zs <3199441913@qq.com>
2026-05-04 18:28:15 +0800

    报警管理页面基本完成

---
+++ src/components/stepForm/stepForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="max-width: 600px">
+    <el-steps :active="currentStep" finish-status="success" align-center>
+      <el-step
+        v-for="(step, index) in steps"
+        :key="index"
+        :title="step.title"
+      ></el-step>
+    </el-steps>
+    <div class="mt" v-if="currentStep == 0">
+      <slot name="step-1"></slot>
+    </div>
+    <div class="mt" v-if="currentStep == 1">
+      <slot name="step-2"></slot>
+    </div>
+    <div class="mt" v-if="currentStep == 2">
+      <slot name="step-3"></slot>
+    </div>
+    <div class="step-button">
+      <el-button v-if="currentStep > 0" @click="currentStep--"
+        >上一步</el-button
+      >
+      <el-button type="primary" @click="nextStep">{{
+        currentStep === steps.length - 1 ? "提交" : "下一步"
+      }}</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps(["steps", "form1", "form2", "form3"]);
+const emit = defineEmits(["submit"]);
+
+const currentStep = ref<number>(0);
+
+const nextStep = () => {
+  const forms = [props.form1, props.form2, props.form3];
+  const currentForm = forms[currentStep.value];
+  currentForm?.validate((valid: boolean) => {
+    if (valid) {
+      if (currentStep.value < props.steps.length - 1) {
+        currentStep.value++;
+      } else {
+        emit("submit");
+      }
+    }
+  });
+};
+</script>
--- src/views/Alarm/alarm.vue
+++ src/views/Alarm/alarm.vue
@@ -30,7 +30,10 @@
         <span v-else>{{ val }}</span>
       </el-descriptions-item>
       <el-descriptions-item label="操作">
-        <el-button :type="item.status == 2 ? 'warning' : 'primary'">
+        <el-button
+          @click="drawer = true"
+          :type="item.status == 2 ? 'warning' : 'primary'"
+        >
           {{ item.status == 1 ? "指派" : item.status == 2 ? "催办" : "查看" }}
         </el-button>
       </el-descriptions-item>
@@ -45,6 +48,110 @@
   <div class="loading-status done" v-else>
     <span>已加载全部告警</span>
   </div>
+
+  <el-drawer v-model="drawer" title="报警任务指派">
+    <div v-loading="loading" style="min-height: 200px">
+      <stepForm
+        :steps="steps"
+        :form1="form1"
+        :form2="form2"
+        :form3="form3"
+        @submit="handleSumbit"
+      >
+        <template #step-1>
+          <el-form :model="formData.basicInfo" :rules="basicRules" ref="form1">
+            <el-form-item label="姓名：" prop="name">
+              <el-input v-model="formData.basicInfo.name"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱：" prop="email">
+              <el-input v-model="formData.basicInfo.email"></el-input>
+            </el-form-item>
+            <el-form-item label="电话：" prop="phone">
+              <el-input v-model="formData.basicInfo.phone"></el-input>
+            </el-form-item>
+            <el-form-item label="工号：" prop="jobNo">
+              <el-input v-model="formData.basicInfo.jobNo"></el-input>
+            </el-form-item>
+            <el-form-item label="是否紧急：">
+              <el-switch v-model="formData.basicInfo.urgent"></el-switch>
+            </el-form-item>
+            <el-form-item>
+              <el-checkbox-group v-model="formData.basicInfo.other">
+                <el-checkbox value="1" label="更换设备"></el-checkbox>
+                <el-checkbox value="2" label="仅维修"></el-checkbox>
+                <el-checkbox value="3" label="需要拍照"></el-checkbox>
+                <el-checkbox value="4" label="需要报备"></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+            <el-form-item label="备注信息：">
+              <el-input
+                type="textarea"
+                placeholder="请输入备注信息"
+                v-model="formData.basicInfo.remarks"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+        </template>
+        <template #step-2>
+          <el-form
+            :model="formData.approvalInfo"
+            ref="form2"
+            :rules="approvalRules"
+          >
+            <el-form-item label="审批部门" prop="approvalRemarks">
+              <el-select
+                placeholder="请选择审批部门"
+                v-model="formData.approvalInfo.approvalRemarks"
+              >
+                <el-option label="总裁办" value="1"></el-option>
+                <el-option label="运营部" value="2"></el-option>
+                <el-option label="维修部" value="3"></el-option>
+                <el-option label="市场部" value="4"></el-option>
+                <el-option label="财务部" value="5"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="抄送部门" prop="approvalResult">
+              <el-select
+                placeholder="请选择抄送部门"
+                v-model="formData.approvalInfo.approvalResult"
+              >
+                <el-option label="总裁办" value="1"></el-option>
+                <el-option label="运营部" value="2"></el-option>
+                <el-option label="维修部" value="3"></el-option>
+                <el-option label="市场部" value="4"></el-option>
+                <el-option label="财务部" value="5"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+        </template>
+        <template #step-3>
+          <el-form
+            :model="formData.assigneeInfo"
+            ref="form3"
+            :rules="assigneeRules"
+          >
+            <el-form-item label="负责人姓名：" prop="assignee">
+              <el-input v-model="formData.assigneeInfo.assignee"></el-input>
+            </el-form-item>
+            <el-form-item label="负责人电话：" prop="assigneePhone">
+              <el-input
+                v-model="formData.assigneeInfo.assigneePhone"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+        </template>
+      </stepForm>
+      <el-result
+        icon="warning"
+        title="设备编号CD1001"
+        sub-title="该任务已催办一次，正在等待维修部门处理"
+      >
+        <template #extra>
+          <el-button type="primary" @click="drawer = false">我已知晓</el-button>
+        </template>
+      </el-result>
+    </div>
+  </el-drawer>
 </template>
 
 <script setup lang="ts">
@@ -52,21 +159,20 @@ import { computed, nextTick, onBeforeUnmount, onMounted, ref } from "vue";
 import { Loading } from "@element-plus/icons-vue";
 import { getAlarmListApi } from "@/api/alarm";
 import { getLabel } from "@/views/Alarm/fileLabelMap";
-
-type AlarmLevel = 1 | 2 | 3;
-type AlarmStatus = 1 | 2 | 3;
+import stepForm from "@/components/stepForm/stepForm.vue";
+import { ElMessage, type FormInstance } from "element-plus";
 
 interface AlarmItem {
   description: string;
   address: string;
   equNo: string;
-  level: AlarmLevel;
+  level: number;
   time: string;
   code: number;
-  status: AlarmStatus;
+  status: number;
 }
 
-const radio = ref<1 | 2 | 3 | 4>(1);
+const radio = ref<number>(1);
 const alarmList = ref<AlarmItem[]>([]);
 
 const PAGE_SIZE = 5;
@@ -83,15 +189,15 @@ const hasMore = computed(() => {
   return visibleCount.value < alarmList.value.length;
 });
 
-const getLevelTagType = (level: AlarmLevel): "danger" | "warning" | "info" => {
+const getLevelTagType = (level: number): "danger" | "warning" | "info" => {
   return level === 1 ? "danger" : level === 2 ? "warning" : "info";
 };
 
-const getLevelText = (level: AlarmLevel): string => {
+const getLevelText = (level: number): string => {
   return level === 1 ? "严重" : level === 2 ? "紧急" : "一般";
 };
 
-const getStatusText = (status: AlarmStatus): string => {
+const getStatusText = (status: number): string => {
   return status === 1 ? "待指派" : status === 2 ? "处理中" : "处理异常";
 };
 
@@ -100,22 +206,17 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const loadMore = async () => {
   if (!hasMore.value || loadingMore.value) return;
   loadingMore.value = true;
-
-  // 给用户明确的“正在加载”反馈（真实分页接口时可移除该延迟）
   await sleep(550);
   await nextTick();
-
   visibleCount.value = Math.min(
     visibleCount.value + PAGE_SIZE,
     alarmList.value.length,
   );
-
   loadingMore.value = false;
 };
 
 const initObserver = () => {
   if (!loadMoreTrigger.value) return;
-
   observer = new IntersectionObserver(
     (entries) => {
       const [entry] = entries;
@@ -129,10 +230,83 @@ const initObserver = () => {
       threshold: 0,
     },
   );
-
   observer.observe(loadMoreTrigger.value);
 };
 
+const drawer = ref<boolean>(false);
+const form1 = ref<FormInstance>();
+const form2 = ref<FormInstance>();
+const form3 = ref<FormInstance>();
+const loading = ref<boolean>(false);
+const steps = [
+  { title: "基本信息" },
+  { title: "审批信息" },
+  { title: "负责人信息" },
+];
+const formData = ref({
+  basicInfo: {
+    name: "",
+    email: "",
+    phone: "",
+    jobNo: "",
+    urgent: true,
+    other: [],
+    remarks: "",
+  },
+  approvalInfo: {
+    approvalResult: "",
+    approvalRemarks: "",
+  },
+  assigneeInfo: {
+    assignee: "",
+    assigneePhone: "",
+  },
+});
+
+const basicRules = {
+  name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+  email: [
+    { required: true, message: "请输入邮箱", trigger: "blur" },
+    { type: "email", message: "请输入正确的邮箱地址", trigger: "blur" },
+  ],
+  phone: [
+    { required: true, message: "请输入电话", trigger: "blur" },
+    {
+      pattern: /^1[3-9]\d{9}$/,
+      message: "请输入有效的手机号",
+      trigger: "blur",
+    },
+  ],
+  jobNo: [{ required: true, message: "请输入工号", trigger: "blur" }],
+};
+const approvalRules = {
+  approvalRemarks: [
+    { required: true, message: "请选择审批部门", trigger: "change" },
+  ],
+  approvalResult: [
+    { required: true, message: "请选择抄送部门", trigger: "change" },
+  ],
+};
+const assigneeRules = {
+  assignee: [{ required: true, message: "请输入指派人", trigger: "blur" }],
+  assigneePhone: [
+    { required: true, message: "请输入指派人电话", trigger: "blur" },
+    {
+      pattern: /^1[3-9]\d{9}$/,
+      message: "请输入有效的手机号",
+      trigger: "blur",
+    },
+  ],
+};
+
+const handleSumbit = () => {
+  loading.value = true;
+  setTimeout(() => {
+    loading.value = false;
+    drawer.value = false;
+    ElMessage.success("指派成功");
+  }, 1000);
+};
 onMounted(async () => {
   const { data } = await getAlarmListApi();
   alarmList.value = data as AlarmItem[];
